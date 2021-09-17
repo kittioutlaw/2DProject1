2f913215b90d6b29b04d42922973ec6e42ed9cf5
kittioutlaw <520195@chrome.escg.ac.uk>
2021-09-17 10:16:14 +0100

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoJump();
+        DoMove();
+
+        void DoJump()
+        {
+            Vector2 velocity = rb.velocity;
+
+            if (Input.GetKey("j"))
+            {
+                print("jump");
+
+                if (velocity.y < 0.01f)
+                {
+                    velocity.y = 24f;
+                }
+            }
+        }
+            rb.velocity = velocity;
+
+        void.DoMove()
+            {
+                Vector2 velocity = rb.velocity
+
+                if (Input.GetKey("z"))
+                {
+                    velocity.x = 1;
+                }
+
+                if (Input.GetKey("x"))
+                {
+                    
+                }
+            }
+  
+  
+    }   
+}
